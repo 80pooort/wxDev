dc5c010878c2bd60f1b2586493c05b570b6fbcd2
wujian <wujian5686@163.com>
2018-10-30 22:22:06 +0800

    add project to repository

---
+++ src/main/java/com/fykj/wxDev/WxDevApplication.java
@@ -0,0 +1,12 @@
+package com.fykj.wxDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxDevApplication.class, args);
+	}
+}
