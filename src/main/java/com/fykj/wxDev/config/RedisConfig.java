898b0eaeb3c4765b2a31850692680c4d78b3fd61
wujian <wujian5686@163.com>
2018-11-07 07:05:14 +0800

    add ...redis底层封装

---
+++ src/main/java/com/fykj/wxDev/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.fykj.wxDev.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redisTemplate 配置类
+ * @Author: wujian
+ * @Date: 2018/11/6 21:02
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
--- src/test/java/com/fykj/wxDev/WxDevApplicationTests.java
+++ src/test/java/com/fykj/wxDev/WxDevApplicationTests.java
@@ -1,7 +1,9 @@
 package com.fykj.wxDev;
 
+import com.fykj.wxDev.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WxDevApplicationTests {
 
+	@Autowired
+	private RedisUtil redisUtil;
+
 	@Test
 	public void contextLoads() {
+//		boolean set = redisUtil.set("gender", "男");
+		Object gender = redisUtil.get("age");
+		System.out.println(gender);
 	}
 
 }
