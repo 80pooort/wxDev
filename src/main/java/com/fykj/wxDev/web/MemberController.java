4dad5a1b2b4950c414af42812890dc6bea569006
wujian <wujian5686@163.com>
2020-06-05 17:39:35 +0800

    Revert 提交测试用例代码
    This reverts commit bff6851

--- src/main/java/com/fykj/wxDev/web/MemberController.java
+++ src/main/java/com/fykj/wxDev/web/MemberController.java
@@ -1,8 +1,28 @@
 package com.fykj.wxDev.web;
 
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * created by wujian on 2019/1/2 9:20
  */
+@RestController
+@RequestMapping("/mem")
+@Slf4j
 public class MemberController extends BaseController {
 
+  @RequestMapping("/hello.json")
+  public void helloWord(HttpServletResponse response){
+    PrintWriter writer = null;
+    try {
+      writer = response.getWriter();
+    } catch (Exception e) {
+      log.error("测试用例异常:{}{}",e.getMessage(),e);
+    }
+    writer.print("hello word");
+
+  }
 }
